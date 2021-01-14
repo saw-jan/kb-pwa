9cc1a5064cd2ac394faf064354bf9658265f4a2a
saw-jan <saw.jan.grg3e@gmail.com>
2021-01-14 21:46:55 +0545

    update webpack to move splashscreens to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,70 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        historyApiFallback: true,
-        port: 3000,
-        hot: true,
-        https: false
-    },
-    entry: './src/index.js',
-    output: {
-        filename: './main.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node-modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.html$/,
-                    use: [
-                        {
-                            loader: 'html-loader',
-                            options: { minimize: true }
-                        }
-                    ]
-            },
-            {
-                test: /\.(ico|png|jpg|gif|eot|ttf|woff|woff2)$/,
-                use: {
-                    loader: 'file-loader'
-                }
-            },
-            {
-                test:/\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.svg$/,
-                use: ['@svgr/webpack'],
-            },
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './src/index.html',
-            filename: './index.html',
-            favicon: './src/favicon.svg'
-        }),
-        new CopyPlugin({
-            patterns: [
-                { from: 'src/sw.js' },
-                { from: 'src/icons/launcher_128.png' },
-                { from: 'src/icons/launcher_512.png' },
-                { from: 'src/icons/launcher_192.png' },
-                { from: 'src/manifest.json' },
-                { from: 'src/offline.html' }
-            ]
-        })
-    ]
-}
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    port: 3000,
+    hot: true,
+    https: false,
+  },
+  entry: './src/index.js',
+  output: {
+    filename: './main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node-modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
+      },
+      {
+        test: /\.(ico|png|jpg|gif|eot|ttf|woff|woff2)$/,
+        use: {
+          loader: 'file-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+      favicon: './src/favicon.svg',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/sw.js' },
+        { from: 'src/icons/launcher_128.png' },
+        { from: 'src/icons/launcher_512.png' },
+        { from: 'src/icons/launcher_192.png' },
+        { from: 'src/manifest.json' },
+        { from: 'src/offline.html' },
+        { from: 'src/splashscreen/' },
+      ],
+    }),
+  ],
+};
