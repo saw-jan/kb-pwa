07f33d35e0996a6e7a2b79e2659459e54c32f548
saw-jan <saw.jan.grg3e@gmail.com>
2021-01-15 17:42:54 +0545

    update service worker

--- src/sw.js
+++ src/sw.js
@@ -4,38 +4,48 @@ let OFFLINE_ASSETS = ['/#/', '/main.js', '/index.html', '/offline.html'];
 // install serviceWorker
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      // initial caching
-      return cache.addAll(OFFLINE_ASSETS);
-    })
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        // initial caching
+        return cache.addAll(OFFLINE_ASSETS);
+      })
+      .then(() => {
+        return self.skipWaiting();
+      })
   );
 });
 
 // activate serviceWorker
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          // clearing old caches
-          return caches.delete(cache);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache
+        .keys()
+        .then((cacheNames) => {
+          return Promise.all(
+            cacheNames.map((cacheName) => {
+              // clearing old cache
+              return caches.delete(cacheName);
+            })
+          );
         })
-      );
+        .then(() => {
+          return self.clients.claim();
+        });
     })
   );
 });
 
 // feed Cached data
 self.addEventListener('fetch', (event) => {
-  console.log(event.request.url);
   event.respondWith(
     caches
       .open(CACHE_NAME)
-      .then(async (cache) => {
+      .then((cache) => {
         // feed from cache
-        const response = await cache.match(event.request);
         return (
-          response ||
+          cache.match(event.request) ||
           fetch(event.request).then((response_1) => {
             // cache new data
             cache.put(event.request, response_1.clone());
